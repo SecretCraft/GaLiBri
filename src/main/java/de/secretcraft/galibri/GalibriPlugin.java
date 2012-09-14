a00f87a02c67787a58c979eac47659adba4b39ac
Linowitch <linowitch@gmail.com>
2012-09-14 12:50:56 +0200

    added gitignore

--- src/main/java/de/secretcraft/galibri/GalibriPlugin.java
+++ src/main/java/de/secretcraft/galibri/GalibriPlugin.java
@@ -2,7 +2,28 @@ package de.secretcraft.galibri;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * 
+ * 
+ * @author sascha thiel
+ */
 public class GalibriPlugin extends JavaPlugin
 {
+	//---------------------------------------------------------------------------------------------
 	
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	//---------------------------------------------------------------------------------------------
+
+	@Override
+	public void onDisable()
+	{
+		
+	}
+	
+	//---------------------------------------------------------------------------------------------
 }
