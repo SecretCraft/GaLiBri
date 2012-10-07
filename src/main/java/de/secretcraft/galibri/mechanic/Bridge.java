6ffcce93b2b05766e0a4875c926cf1b00c181e79
Linowitch <linowitch@gmail.com>
2012-10-07 21:21:38 +0200

    working on the bridge

--- src/main/java/de/secretcraft/galibri/mechanic/Bridge.java
+++ src/main/java/de/secretcraft/galibri/mechanic/Bridge.java
@@ -1,5 +1,12 @@
 package de.secretcraft.galibri.mechanic;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.SignChangeEvent;
@@ -7,46 +14,215 @@ import org.bukkit.event.block.SignChangeEvent;
 import de.secretcraft.galibri.GalibriPlugin;
 
 /**
- * 
+ * This class handles all Bridge signs that will be created
  * 
  * @author sascha thiel
  */
 public class Bridge extends AbstractMechanic
 {
-	//---------------------------------------------------------------------------------------------
-	
+	// ---------------------------------------------------------------------------------------------
 
-	
-	//---------------------------------------------------------------------------------------------
-	
+	protected enum SetFacingCode {
+		WALL_SIGN, DIRECTION, EVERYTHING_OK;
+	}
+
+	public final static String CONFIG_BLOCKS_PATH = "bridge.blocks";
+	public final static String CONFIG_BRIDGE_MAX_SIZE_PATH = "bridge.max-size";
+
+	// ---------------------------------------------------------------------------------------------
+
 	public Bridge(GalibriPlugin plugin)
 	{
 		super(plugin);
 		permissions.put(Perm.INITIALIZE, "galibri.bridge.create");
 		permissions.put(Perm.DO_ACTION, "galibri.bridge.use");
 	}
-	
-	//---------------------------------------------------------------------------------------------
+
+	// ---------------------------------------------------------------------------------------------
 	@Override
 	public boolean initialize(SignChangeEvent event)
 	{
-		if(!super.initialize(event)) return false;
+		if (!super.initialize(event))
+			return false;
 		final Player player = event.getPlayer();
+		Sign sign = (Sign) event.getBlock().getState();
+		switch (setFacingDirection(sign)) {
+		case DIRECTION:
+			// TODO: STH localize
+			player.sendMessage("Could you please set a proper direction?");
+			event.setLine(1, "[direction?]");
+			return false;
+		case WALL_SIGN:
+			// TODO: STH localize
+			player.sendMessage("Bridges can not be on a wall");
+			event.setLine(1, "[not possible]");
+			return false;
+		default:
+		}
 		event.setLine(1, "[Bridge]");
+		event.setLine(3, "0");
 		player.sendMessage(event.getLine(1) + " sign created");
 		return true;
 	}
-	
-	//---------------------------------------------------------------------------------------------
+
+	// ---------------------------------------------------------------------------------------------
 	@Override
 	public boolean doAction(Sign sign, Player player)
 	{
-		if(!super.doAction(sign, player)) return false;
-		
-		player.sendMessage("Bridge => doAction "+ player.getName());
-		
+		if (!super.doAction(sign, player))
+			return false;
+
+		int maxLength = plugin.getConfig().getInt(CONFIG_BRIDGE_MAX_SIZE_PATH + ".length");
+		int maxWidth = plugin.getConfig().getInt(CONFIG_BRIDGE_MAX_SIZE_PATH + ".width");
+		Sign partnerSign = searchForPartnerSign(sign);
+		if (partnerSign == null) {
+			// TODO: STH localize
+			player.sendMessage("no partner sign found (max " + String.valueOf(maxLength) + " Blocks)");
+			return false;
+		}
+		List<Block> startBlocks = getStartBlocks(sign, maxWidth);
+		if(startBlocks == null){
+			player.sendMessage("no bridge blocks found");
+			return false;
+		}
+		int blocksAvailable = Integer.valueOf(sign.getLine(3));
+		if(blocksAvailable == 0) {
+			
+		} else {
+			
+		}
+
 		return true;
 	}
+
+	// ---------------------------------------------------------------------------------------------
+
+	private SetFacingCode setFacingDirection(final BlockState signState)
+	{
+		org.bukkit.material.Sign signData = (org.bukkit.material.Sign) signState.getData();
+		if (signData.isWallSign())
+			return SetFacingCode.WALL_SIGN;
+		switch (signData.getFacing()) {
+		case EAST_NORTH_EAST:
+		case EAST_SOUTH_EAST:
+			signData.setFacingDirection(BlockFace.EAST);
+			signState.update();
+		case EAST:
+			break;
+		case NORTH_NORTH_EAST:
+		case NORTH_NORTH_WEST:
+			signData.setFacingDirection(BlockFace.NORTH);
+			signState.update();
+		case NORTH:
+			break;
+		case SOUTH_SOUTH_EAST:
+		case SOUTH_SOUTH_WEST:
+			signData.setFacingDirection(BlockFace.SOUTH);
+			signState.update();
+		case SOUTH:
+			break;
+		case WEST_NORTH_WEST:
+		case WEST_SOUTH_WEST:
+			signData.setFacingDirection(BlockFace.WEST);
+			signState.update();
+		case WEST:
+			break;
+		default:
+			return SetFacingCode.DIRECTION;
+		}
+		return SetFacingCode.EVERYTHING_OK;
+	}
+
+	// ---------------------------------------------------------------------------------------------
+
+	private Sign searchForPartnerSign(final Sign sign)
+	{
+		Sign partnerSign = null;
+		org.bukkit.material.Sign signData = (org.bukkit.material.Sign) sign.getData();
+		Block searchBlock = sign.getBlock();
+		int maxLength = plugin.getConfig().getInt(CONFIG_BRIDGE_MAX_SIZE_PATH + ".length");
+		for (int i = 0; i < maxLength; ++i) {
+			searchBlock = searchBlock.getRelative(signData.getFacing().getOppositeFace());
+			Material mat = searchBlock.getType();
+			if (mat == Material.SIGN || mat == Material.SIGN_POST) {
+				if (((Sign) searchBlock.getState()).getLine(1).equalsIgnoreCase("[Bridge]")) {
+					partnerSign = (Sign) searchBlock.getState();
+					break;
+				}
+			}
+		}
+		return partnerSign;
+	}
 	
-	//---------------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------------
+	
+	protected List<Block> getStartBlocks(final Sign sign, final int maxWidth)
+	{
+		final List<Block> blocks = new ArrayList<Block>();
+		
+		Block blockUnderSign = sign.getBlock().getRelative(BlockFace.DOWN);
+		Block blockA = blockUnderSign;
+		Block blockB = blockUnderSign;
+		if(isBridgeBlock(blockUnderSign)) blocks.add(blockUnderSign);
+		else return null;
+		
+		List<BlockFace> faces = getCrossedFaces((org.bukkit.material.Sign) sign.getData());
+		if(faces == null) return null;
+		
+		for(int i = 0;i<maxWidth/2;++i){
+			blockA = blockA.getRelative(faces.get(0));
+			blockB = blockB.getRelative(faces.get(1));
+			
+			if(isBridgeBlock(blockA)) blocks.add(blockA);
+			if(isBridgeBlock(blockB)) blocks.add(blockB);
+		}
+		
+		return blocks;
+	}
+	
+	// ---------------------------------------------------------------------------------------------
+	
+	protected List<BlockFace> getCrossedFaces(org.bukkit.material.Sign signData)
+	{
+		List<BlockFace> faces = new ArrayList<BlockFace>();
+		switch(signData.getFacing().getOppositeFace()){
+			case NORTH:
+			case SOUTH:
+				faces.add(BlockFace.WEST);
+				faces.add(BlockFace.EAST);
+			break;
+			case EAST:
+			case WEST:
+				faces.add(BlockFace.NORTH);
+				faces.add(BlockFace.SOUTH);
+			break;
+			default:
+				return null;
+		}
+		return faces;
+	}
+
+	// ---------------------------------------------------------------------------------------------
+
+	protected boolean isBridgeBlock(Block block)
+	{
+		List<Integer> blockList = plugin.getConfig().getIntegerList(CONFIG_BLOCKS_PATH);
+		return blockList.contains(block.getType().getId());
+	}
+	
+	// ---------------------------------------------------------------------------------------------
+
+	protected int createBridge(final List<Block> startBlocks, final Material bridgeMat, final BlockFace face, final int length)
+	{
+		int blockCount = 0;
+		for(int i=0;i<length;++i) {
+			for(Block currentBlock : startBlocks) {
+				currentBlock.setType(bridgeMat);
+				currentBlock = currentBlock.getRelative(face);
+			}
+		}
+		return blockCount;
+	}
+
+	// ---------------------------------------------------------------------------------------------
 }
--- src/main/java/de/secretcraft/galibri/mechanic/Gate.java
+++ src/main/java/de/secretcraft/galibri/mechanic/Gate.java
@@ -22,117 +22,124 @@ import de.secretcraft.galibri.GalibriPlugin;
  */
 public class Gate extends AbstractMechanic
 {
-	//---------------------------------------------------------------------------------------------
-	
-	public final static String CONFIG_BLOCKS_PATH = "gate-blocks";
-	public final static String CONFIG_ARCHWAY_BLOCK_PATH = "gate-archway-blocks";
-	public final static String CONFIG_SEARCH_OFFSET = "gate-search-range";
-	
-	//---------------------------------------------------------------------------------------------
-	
+	// ---------------------------------------------------------------------------------------------
+
+	public final static String CONFIG_BLOCKS_PATH = "gate.blocks";
+	public final static String CONFIG_ARCHWAY_BLOCK_PATH = "gate.archway-blocks";
+	public final static String CONFIG_SEARCH_OFFSET = "gate.search-range";
+
+	// ---------------------------------------------------------------------------------------------
+
 	public Gate(final GalibriPlugin plugin)
 	{
 		super(plugin);
 		permissions.put(Perm.INITIALIZE, "galibri.gate.create");
 		permissions.put(Perm.DO_ACTION, "galibri.gate.use");
 	}
-	
-	//---------------------------------------------------------------------------------------------
+
+	// ---------------------------------------------------------------------------------------------
 	@Override
 	public boolean initialize(final SignChangeEvent event)
 	{
-		if(!super.initialize(event)) return false;
+		if (!super.initialize(event))
+			return false;
 		final Player player = event.getPlayer();
 		event.setLine(1, "[Gate]");
 		player.sendMessage(event.getLine(1) + " sign created");
 		return true;
 	}
-	
-	//---------------------------------------------------------------------------------------------
+
+	// ---------------------------------------------------------------------------------------------
 	@Override
 	public boolean doAction(final Sign sign, final Player player)
 	{
-		if(!super.doAction(sign, player)) return false;
+		if (!super.doAction(sign, player))
+			return false;
 
 		List<Block> topBlocks = searchForTopBlocks(sign.getBlock());
-		if(topBlocks.size() == 0){
+		if (topBlocks.size() == 0) {
 			player.sendMessage("no gate blocks found");
 			return false;
 		}
 		toggleGate(topBlocks);
 		return true;
 	}
-	
-	//---------------------------------------------------------------------------------------------
-	
+
+	// ---------------------------------------------------------------------------------------------
+
 	protected List<Block> searchForTopBlocks(Block block)
 	{
 		List<Block> back = new ArrayList<Block>();
-		final YamlConfiguration conf = (YamlConfiguration)plugin.getConfig();
-		final Integer xOff = conf.getInt(CONFIG_SEARCH_OFFSET+".x");
-		final Integer yOff = conf.getInt(CONFIG_SEARCH_OFFSET+".y");
-		final Integer zOff = conf.getInt(CONFIG_SEARCH_OFFSET+".z");
-		
-		final Location search = new Location(block.getWorld(), block.getX()-xOff, block.getY()+yOff, block.getZ()-zOff);
+		final YamlConfiguration conf = (YamlConfiguration) plugin.getConfig();
+		final Integer xOff = conf.getInt(CONFIG_SEARCH_OFFSET + ".x");
+		final Integer yOff = conf.getInt(CONFIG_SEARCH_OFFSET + ".y");
+		final Integer zOff = conf.getInt(CONFIG_SEARCH_OFFSET + ".z");
+
+		final Location search = new Location(block.getWorld(), block.getX() - xOff, block.getY() + yOff, block.getZ()
+				- zOff);
 		final World world = block.getWorld();
-		
+
 		boolean foundGateBlock = false;
-		
-		for(int yLoc = 0; yLoc < yOff * 2; ++yLoc){
-			for(int xLoc = 0; xLoc < xOff * 2; ++xLoc){
-				for(int zLoc = 0; zLoc < zOff * 2; ++zLoc){
-					Block currentBlock = world.getBlockAt(search.getBlockX() + xLoc, search.getBlockY() - yLoc, search.getBlockZ() + zLoc);
-					if(isGateBlock(currentBlock) && isInArchway(currentBlock)) {
+
+		for (int yLoc = 0; yLoc < yOff * 2; ++yLoc) {
+			for (int xLoc = 0; xLoc < xOff * 2; ++xLoc) {
+				for (int zLoc = 0; zLoc < zOff * 2; ++zLoc) {
+					Block currentBlock = world.getBlockAt(search.getBlockX() + xLoc, search.getBlockY() - yLoc,
+							search.getBlockZ() + zLoc);
+					if (isGateBlock(currentBlock) && isInArchway(currentBlock)) {
 						foundGateBlock = true;
 						back.add(currentBlock);
 					}
 				}
 			}
 			// do not search in the next area if a gate block was found
-			if(foundGateBlock) break;
+			if (foundGateBlock)
+				break;
 		}
 		return back;
 	}
-	
-	//---------------------------------------------------------------------------------------------
-	
+
+	// ---------------------------------------------------------------------------------------------
+
 	protected void toggleGate(List<Block> topBlocks)
 	{
 		Material gateMat = topBlocks.get(0).getType();
 		Material toggleMat = null;
-		for(Block block : topBlocks){
+		for (Block block : topBlocks) {
 			boolean end = false;
-			while(!end) {
+			while (!end) {
 				block = block.getRelative(BlockFace.DOWN);
-				if(block.getType() == Material.AIR) {
-					if(toggleMat == null) toggleMat = gateMat;
+				if (block.getType() == Material.AIR) {
+					if (toggleMat == null)
+						toggleMat = gateMat;
 					block.setType(toggleMat);
-				} else if(block.getType() == gateMat) {
-					if(toggleMat == null) toggleMat = Material.AIR;
+				} else if (block.getType() == gateMat) {
+					if (toggleMat == null)
+						toggleMat = Material.AIR;
 					block.setType(toggleMat);
 				} else {
 					end = true;
-				}					
+				}
 			}
 		}
 	}
-	
-	//---------------------------------------------------------------------------------------------
-	
+
+	// ---------------------------------------------------------------------------------------------
+
 	protected boolean isGateBlock(final Block block)
 	{
 		List<Integer> blockList = plugin.getConfig().getIntegerList(CONFIG_BLOCKS_PATH);
 		return blockList.contains(block.getType().getId());
 	}
-	
-	//---------------------------------------------------------------------------------------------
-	
+
+	// ---------------------------------------------------------------------------------------------
+
 	protected boolean isInArchway(final Block block)
 	{
 		Block onTopOfIt = block.getRelative(BlockFace.UP);
 		List<Integer> blockList = plugin.getConfig().getIntegerList(CONFIG_ARCHWAY_BLOCK_PATH);
 		return blockList.contains(onTopOfIt.getType().getId());
 	}
-	
-	//---------------------------------------------------------------------------------------------
+
+	// ---------------------------------------------------------------------------------------------
 }
